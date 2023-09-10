dbdddb98e495a1f011f23ababd69d411496efa19
jagadeeshvarma1 <70195354+jagadeeshvarma1@users.noreply.github.com>
2023-09-10 18:19:24 +0530

    Add files via upload

---
+++ WORDLE.py
@@ -0,0 +1,14 @@
+# cook your dish here
+t = int(input())
+ans = ''
+
+for i in range(t):
+    s = list(input())
+    t = list(input())
+    for j in range(len(s)):
+            if s[j] == t[j]:
+                ans += 'G'
+            else:
+                ans += 'B'
+    print(ans)
+    ans = ''
