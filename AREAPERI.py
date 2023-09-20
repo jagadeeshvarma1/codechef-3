2be297c51ca5a5f13fb2951cf912314a1ff1b659
jagadeeshvarma1 <70195354+jagadeeshvarma1@users.noreply.github.com>
2023-09-20 18:43:34 +0530

    Add files via upload

---
+++ AREAPERI.py
@@ -0,0 +1,13 @@
+l=int(input())
+b=int(input())
+area = l*b
+peri = 2*(l+b)
+if area<peri:
+    print("Peri")
+    print(peri)
+elif area>peri:
+    print("Area")
+    print(area)
+else:
+    print("Eq")
+    print(area)
