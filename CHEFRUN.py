f88374fd1f30846bdc72edb46a3fe402fb82e407
jagadeeshvarma1 <70195354+jagadeeshvarma1@users.noreply.github.com>
2023-09-28 19:19:17 +0530

    Add files via upload

---
+++ CHEFRUN.py
@@ -0,0 +1,12 @@
+# cook your dish here
+t=int(input())
+for i in range(t):
+    x1,x2,x3,v1,v2=map(int,input().split())
+    if((x3 - x1) / v1  > (x2 - x3) / v2):
+        print("Kefa")
+    elif((x3 - x1) / v1 < (x2 - x3) / v2):
+        print("Chef")
+    else:
+        print("Draw")
+
+    
