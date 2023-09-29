ccff5d30f3e0d209138067cb613656ebf8d53365
jagadeeshvarma1 <70195354+jagadeeshvarma1@users.noreply.github.com>
2023-09-29 12:09:40 +0530

    Add files via upload

---
+++ AGENTCHEF.py
@@ -0,0 +1,5 @@
+import math
+for _ in range(int(input())):
+    x=int(input())
+    z=(x*20)/100
+    print(math.ceil(100/z))
---
+++ NEWSPAPER.py
@@ -0,0 +1,8 @@
+x=int(input())
+for i in range(1,10):
+    if(x==8 or x==9 or x==10):
+        print("YES")
+        break
+    else:
+        print("NO")
+        break
